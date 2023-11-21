7d4b82f6c1ecdf5e2ef19f8f7ad2b71498458772
seung.bok <seungbok.lee@huinnoaim.com>
2023-11-21 17:24:24 +0900

    feat: react 바인딩 추가

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {useState} from 'react'
+import kkobuki from './assets/kko.png'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0)
+    const [inputText, setInputText] = useState('')
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const handleInputChange = (event) => {
+        setInputText(event.target.value);
+    };
+
+    return (
+        <>
+            <div>
+                <a href="https://code-boki.tistory.com/" target="_blank">
+                    <img src={kkobuki} className="logo" alt="Blog"/>
+                </a>
+            </div>
+            <h1>Vite + React</h1>
+            <h2> from Boki</h2>
+            <div className="card">
+                <button onClick={() => setCount((count) => count + 1)}>
+                    count is {count}
+                </button>
+            </div>
+            <h2>Two Way Binding Text Input</h2>
+            <input id="my-input" type="text" placeholder="Enter text here" value={inputText} onChange={handleInputChange}/>
+            <p id="string-display">{inputText ? inputText : 'Initial value'}</p>
+        </>
+    )
 }
 
 export default App
