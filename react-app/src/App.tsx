f206b319c1cd23194cc5977c0f42320d000294eb
seung.bok <seungbok.lee@huinnoaim.com>
2023-11-27 11:02:57 +0900

    fix: react 단방향 바인딩 추가, 스타일링

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -5,14 +5,19 @@ import './App.css'
 function App() {
     const [count, setCount] = useState(0)
     const [inputText, setInputText] = useState('')
+    const [look, setLook] = useState('Not handsome')
 
     const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
         setInputText(event.target.value)
     }
 
+    const lookStyle = () => {
+        return look === 'Not handsome' ? 'yellow' : 'green';
+    }
+
     return (
         <>
-            <div>
+            <div className="center">
                 <a href="https://code-boki.tistory.com/" target="_blank">
                     <img src={kkobuki} className="logo" alt="Blog"/>
                 </a>
@@ -24,9 +29,18 @@ function App() {
                     count is {count}
                 </button>
             </div>
-            <h2>Two Way Binding Text Input</h2>
-            <input id="my-input" type="text" placeholder="Enter text here" value={inputText} onChange={handleInputChange}/>
-            <p className="string-display">{inputText ? inputText : 'Initial value'}</p>
+            <div className="card">
+                <h2>One Way Binding</h2>
+                <button type="button" onClick={() => setLook('handsome')}>
+                    잘생겨지게 만들기
+                </button>
+                <h3 style={{ color: lookStyle() }}>{look}</h3>
+            </div>
+            <div className="card">
+                <h2>Two Way Binding Text Input</h2>
+                <input id="my-input" type="text" placeholder="Enter text here" value={inputText} onChange={handleInputChange}/>
+                <p className="string-display">{inputText ? inputText : 'Initial value'}</p>
+            </div>
         </>
     )
 }
