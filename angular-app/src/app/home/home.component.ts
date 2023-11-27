3858adc5aa7256ef4c7c3b9f486c43dcc2184655
seung.bok <seungbok.lee@huinnoaim.com>
2023-11-27 10:40:47 +0900

    fix: angular 단방향 바인딩 추가

--- angular-app/src/app/home/home.component.ts
+++ angular-app/src/app/home/home.component.ts
@@ -8,12 +8,22 @@ import { Component } from '@angular/core';
 export class HomeComponent {
   count: number
   text: string
+  look: string
   constructor() {
     this.count = 0
     this.text = ''
+    this.look = 'Not handsome'
+  }
+
+  lookStyle() {
+    return this.look === 'Not handsome' ? 'yellow' : 'green'
   }
 
   addCounter() {
     this.count += 1
+  }
+
+  changeLook() {
+    this.look = 'handsome'
   }
 }
